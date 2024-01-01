0c1c3de2e6f17815394c7090a5ee1543441f0d2b
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2024-01-02 00:27:21 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("Hellooooooooo! from external javascript")
+
+// input
+var useName = prompt("What's your name?");
+
+// processing
+var welcomeMessage = "Welcome! " + useName;
+
+// output
+alert(welcomeMessage);
+
+// read about history based routing  in vanilla js
+// read about reconcilation
+// creatr your own version of react redux, react router
