9aa716c031f8fcebf9e3bb0621e3aab55d59aa89
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2024-01-02 03:27:17 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-// console.log("Hellooooooooo! from external javascript")
+// // console.log("Hellooooooooo! from external javascript")
 
-// input
-var useName = prompt("What's your name?");
+// // input
+// var useName = prompt("What's your name?");
 
-// processing
-var welcomeMessage = "Welcome! " + useName;
+// // processing
+// var welcomeMessage = "Welcome! " + useName;
 
-// output
-alert(welcomeMessage);
+// // output
+// alert(welcomeMessage);
 
-// read about history based routing  in vanilla js
-// read about reconcilation
-// creatr your own version of react redux, react router
+// // read about history based routing  in vanilla js
+// // read about reconcilation
+// // creatr your own version of react redux, react router
+
+/**
+ * Topic: 
+ * - Add js in html
+ * - querySelector
+ * - Event Listener
+ * - use selectors to get value (.value)
+ * - FAQ: what is API, how query selector works, What is GUI
+ */
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+
+// btnTranslate.addEventListener("click", function clickEventhandler() {
+//     console.log("clicked");
+// }) OR
+
+
+function clickHandler() {
+    console.log("clicked");
+    console.log("input", txtInput.value)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
